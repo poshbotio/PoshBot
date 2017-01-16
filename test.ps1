72d414f7cb76436a5e692330c67dac1464d08489
Brandon Olin <brandon@devblackops.io>
2017-01-16 15:57:31 -0800

    Create the configuration before creating bot

--- test.ps1
+++ test.ps1
@@ -5,12 +5,10 @@ Import-Module $PSScriptroot\PoshBot.psd1 -Force -Verbose:$false
 $VerbosePreference = 'continue'
 $DebugPreference = 'continue'
 
+# Create the configuration
+$config = New-PoshBotConfiguration -BotAdmins 'bolin' -CommandPrefix '!' -LogLevel 'Debug' -AlternateCommandPrefixes 'bender', 'hal' -LogDirectory (Join-Path -Path $env:USERPROFILE -ChildPath '.poshbot\Logs')
+$configFile = $config | Save-PoshBotConfiguration -PassThru
+
 # Create a Slack backend and bot instance
 $backend = New-PoshBotSlackBackend -Name 'SlackBackend' -BotToken $env:SLACK_TOKEN
-$bot = New-PoshBotInstance -Name 'SlackBot' -Backend $backend -ConfigurationDirectory (Join-Path -Path $env:USERPROFILE -ChildPath '.poshbot')
-
-# $moduleName = 'demo'
-# $manifestPath = "$PSScriptRoot\Plugins\$moduleName\$($moduleName).psd1"
-# $bot | Add-PoshBotPlugin -ModuleManifest $manifestPath
-
-#$bot.Start()
+$bot = New-PoshBotInstance -Name 'SlackBot' -Backend $backend -ConfigurationDirectory $configFile.Directory.FullName
