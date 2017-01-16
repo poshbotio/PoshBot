063b72e0014406d42363b2e74507f7eba4fc9a85
Brandon Olin <brandon@devblackops.io>
2017-01-15 22:29:20 -0800

    Adjust test script to use new config directory parameter

--- test.ps1
+++ test.ps1
@@ -7,10 +7,10 @@ $DebugPreference = 'continue'
 
 # Create a Slack backend and bot instance
 $backend = New-PoshBotSlackBackend -Name 'SlackBackend' -BotToken $env:SLACK_TOKEN
-$bot = New-PoshBotInstance -Name 'SlackBot' -Backend $backend
+$bot = New-PoshBotInstance -Name 'SlackBot' -Backend $backend -ConfigurationDirectory (Join-Path -Path $env:USERPROFILE -ChildPath '.poshbot')
 
 $moduleName = 'demo'
 $manifestPath = "$PSScriptRoot\Plugins\$moduleName\$($moduleName).psd1"
 $bot | Add-PoshBotPlugin -ModuleManifest $manifestPath
 
-$bot.Start()
+#$bot.Start()
