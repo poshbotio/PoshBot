ce6e207ab8cab8718cc1b55fd762d526243689c1
Brandon Olin <brandon@devblackops.io>
2017-01-21 15:11:25 -0800

    Add a motivational squirrel command

--- Plugins/Demo/Demo.psm1
+++ Plugins/Demo/Demo.psm1
@@ -173,3 +173,33 @@ function Roll-Dice {
         Total = ([int]$bonus + $total)
     }
 }
+
+function Shipit {
+    <#
+    .SYNOPSIS
+        Display a motivational squirrel
+    .EXAMPLE
+        !shipit
+    #>
+    [cmdletbinding()]
+    param()
+
+    $squirrels = @(
+        'http://28.media.tumblr.com/tumblr_lybw63nzPp1r5bvcto1_500.jpg',
+        'http://i.imgur.com/DPVM1.png',
+        'http://d2f8dzk2mhcqts.cloudfront.net/0772_PEW_Roundup/09_Squirrel.jpg',
+        'http://www.cybersalt.org/images/funnypictures/s/supersquirrel.jpg',
+        'http://www.zmescience.com/wp-content/uploads/2010/09/squirrel.jpg',
+        'https://dl.dropboxusercontent.com/u/602885/github/sniper-squirrel.jpg',
+        'http://1.bp.blogspot.com/_v0neUj-VDa4/TFBEbqFQcII/AAAAAAAAFBU/E8kPNmF1h1E/s640/squirrelbacca-thumb.jpg',
+        'https://dl.dropboxusercontent.com/u/602885/github/soldier-squirrel.jpg',
+        'https://dl.dropboxusercontent.com/u/602885/github/squirrelmobster.jpeg',
+        'http://i.imgur.com/tIQluOd.jpg"',
+        'http://i.imgur.com/PIQBHKA.jpg',
+        'http://i.imgur.com/Qp8iF6l.jpg',
+        'http://i.imgur.com/I7drYFb.jpg',
+        'http://i.imgur.com/1obU7mz.jpg'
+    )
+
+    Write-Output $squirrels | Get-Random
+}
