3921ce3c1e31756f62c3bdb05eeba7254b86406f
Brandon Olin <brandon@devblackops.io>
2017-01-22 16:13:38 -0800

    Add a public custom attribute so modules can decorate functions/cmdlets with custom metadata about how the bot should process the command

--- PoshBot.psm1
+++ PoshBot.psm1
@@ -5,6 +5,46 @@ $script:botTracker = @{
 
 }
 
+# Out custom attribute that external modules can decorate
+# command with. This controls the command behavior when imported
+Add-Type -TypeDefinition @"
+namespace PoshBot {
+
+    public enum TriggerType {
+        Command,
+        Regex,
+        Timer
+    }
+
+    public class BotCommand : System.Attribute {
+
+        public string CommandName { get; set; }
+        public bool HideFromHelp { get; set; }
+
+        public string Regex { get; set; }
+
+        private TriggerType _triggerType = TriggerType.Command;
+        private bool _command = true;
+        private bool _keepHistory = true;
+
+        public TriggerType TriggerType {
+            get { return _triggerType; }
+            set { _triggerType = value; }
+        }
+
+        public bool Command {
+            get { return _command; }
+            set { _command = value; }
+        }
+
+        public bool KeepHistory {
+            get { return _keepHistory; }
+            set { _keepHistory = value; }
+        }
+    }
+}
+"@
+
 # Base classes and associated functions
 @(
     'Logger'
