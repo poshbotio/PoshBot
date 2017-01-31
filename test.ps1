8fa90a223772264555361e7cd53eb9aa605e67fa
Brandon Olin <brandon@devblackops.io>
2017-01-30 21:46:51 -0800

    Test changes

--- test.ps1
+++ test.ps1
@@ -5,25 +5,29 @@ Import-Module $PSScriptroot\PoshBot.psd1 -Force -Verbose:$false
 $VerbosePreference = 'continue'
 #$DebugPreference = 'continue'
 
-# Create a Slack backend
-$backend = New-PoshBotSlackBackend -Name 'SlackBackend' -BotToken $env:SLACK_TOKEN
-
-# Start a interactive bot from a newly created configuration
+# # Start a interactive bot from a newly created configuration
 $botParams = @{
     Name = 'Cherry2000'
     BotAdmins = 'bolin'
     CommandPrefix = '!'
     LogLevel = 'Debug'
+    BackendConfiguration = @{
+        Token = $env:SLACK_TOKEN
+        Name = 'SlackBackend'
+    }
     AlternateCommandPrefixes = 'bender', 'hal'
     LogDirectory = (Join-Path -Path $env:USERPROFILE -ChildPath '.poshbot\Logs')
 }
 $config = New-PoshBotConfiguration @botParams
+
+# Create a Slack backend
+$backend = New-PoshBotSlackBackend -Configuration $config.BackendConfiguration
 $configFile = $config | Save-PoshBotConfiguration -Path (Join-Path -Path $env:USERPROFILE -ChildPath '.poshbot\Cherry2000.psd1') -PassThru
 #$config | Start-PoshBot
 
 # # Get an existing configuration and start interactive bot
-# $config = Get-PoshBotConfiguration -Path (Join-Path -Path $env:USERPROFILE -ChildPath '.poshbot\Cherry2000.psd1')
-# $config | Start-PoshBot
+#$config = Get-PoshBotConfiguration -Path (Join-Path -Path $env:USERPROFILE -ChildPath '.poshbot\Cherry2000.psd1')
+#$config | Start-PoshBot
 
 # Start-PoshBot -ConfigurationPath (Join-Path -Path $env:USERPROFILE -ChildPath '.poshbot\Cherry2000.psd1')
 
