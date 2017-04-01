9dfc1113c813878f719d4c593ade0f1519ee59ce
Brandon Olin <brandon@devblackops.io>
2017-04-01 14:39:49 -0700

    Improve module dependency installation

--- build.ps1
+++ build.ps1
@@ -5,51 +5,77 @@ param(
     [string[]]$Task = 'default',
 
     [parameter(ParameterSetName = 'help')]
-    [switch]$Help
+    [switch]$Help,
+
+    [switch]$UpdateModules
 )
 
 function Resolve-Module {
     [Cmdletbinding()]
     param (
         [Parameter(Mandatory, ValueFromPipeline)]
-        [string[]]$Name
+        [string[]]$Name,
+
+        [switch]$UpdateModules
     )
 
-    Process {
-        foreach ($ModuleName in $Name) {
-            $Module = Get-Module -Name $ModuleName -ListAvailable -Verbose:$false
-            Write-Verbose -Message "Resolving Module [$($ModuleName)]"
+    begin {
+        Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
+        Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
-            if ($Module) {
-                $Version = $Module | Measure-Object -Property Version -Maximum | Select-Object -ExpandProperty Maximum
-                $GalleryVersion = Find-Module -Name $ModuleName -Repository PSGallery -Verbose:$false |
-                    Measure-Object -Property Version -Maximum |
-                    Select-Object -ExpandProperty Maximum
+        $PSDefaultParameterValues = @{
+            '*-Module:Verbose' = $false
+            'Install-Module:ErrorAction' = 'Stop'
+            'Install-Module:Force' = $true
+            'Install-Module:Scope' = 'CurrentUser'
+            'Install-Module:AllowClobber' = $true
+            'Import-Module:ErrorAction' = 'Stop'
+            'Import-Module:Force' = $true
+        }
+    }
 
-                if ($Version -lt $GalleryVersion) {
-                    Write-Verbose -Message "$($ModuleName) Installed Version [$($Version.tostring())] is outdated. Installing Gallery Version [$($GalleryVersion.tostring())]"
+    process {
+        foreach ($moduleName in $Name) {
+            $versionToImport = ''
 
-                    Install-Module -Name $ModuleName -Verbose:$false -Force
-                    Import-Module -Name $ModuleName -Verbose:$false -Force -RequiredVersion $GalleryVersion
+            Write-Verbose -Message "Resolving Module [$($moduleName)]"
+            if ($Module = Get-Module -Name $moduleName -ListAvailable) {
+                # Determine latest version on PSGallery and warn us if we're out of date
+                $latestLocalVersion = ($Module | Measure-Object -Property Version -Maximum).Maximum
+                $latestGalleryVersion = (Find-Module -Name $moduleName -Repository PSGallery |
+                    Measure-Object -Property Version -Maximum).Maximum
+
+                # Out we out of date?
+                if ($latestLocalVersion -lt $latestGalleryVersion) {
+                    if ($UpdateModules) {
+                        Write-Verbose -Message "$($moduleName) installed version [$($latestLocalVersion.ToString())] is outdated. Installing gallery version [$($latestGalleryVersion.ToString())]"
+                        if ($UpdateModules) {
+                            Install-Module -Name $moduleName -RequiredVersion $latestGalleryVersion
+                            $versionToImport = $latestGalleryVersion
+                        }
+                    } else {
+                        Write-Warning "$($moduleName) is out of date. Latest version on PSGallery is [$latestGalleryVersion]. To update, use the -UpdateModules switch."
+                    }
+                } else {
+                    $versionToImport = $latestLocalVersion
                 }
-                else {
-                    Write-Verbose -Message "Module Installed, Importing [$($ModuleName)]"
-                    Import-Module -Name $ModuleName -Verbose:$false -Force -RequiredVersion $Version
-                }
+            } else {
+                Write-Verbose -Message "[$($moduleName)] missing. Installing..."
+                Install-Module -Name $moduleName -Repository PSGallery
+                $versionToImport = (Get-Module -Name $moduleName -ListAvailable | Measure-Object -Property Version -Maximum).Maximum
             }
-            else {
-                Write-Verbose -Message "[$($ModuleName)] Missing, installing Module"
-                Install-Module -Name $ModuleName -Verbose:$false -Force
-                Import-Module -Name $ModuleName -Verbose:$false -Force -RequiredVersion $Version
+
+            Write-Verbose -Message "$($moduleName) installed. Importing..."
+            if (-not [string]::IsNullOrEmpty($versionToImport)) {
+                Import-module -Name $moduleName -RequiredVersion $versionToImport
+            } else {
+                Import-module -Name $moduleName
             }
         }
     }
 }
 
-Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-
-'BuildHelpers', 'psake' | Resolve-Module
+'BuildHelpers', 'psake' | Resolve-Module -UpdateModules:($PSBoundParameters.ContainsKey('UpdateModules'))
 
 if ($PSBoundParameters.ContainsKey('help')) {
     Get-PSakeScriptTasks -buildFile "$PSScriptRoot\psake.ps1" |
