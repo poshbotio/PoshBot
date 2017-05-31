8ff852996eabd63189014518c854e95f00d90738
Brandon Olin <brandon@devblackops.io>
2017-05-30 22:40:39 -0700

    Only warn on missing config files

--- PoshBot/Classes/StorageProvider.ps1
+++ PoshBot/Classes/StorageProvider.ps1
@@ -18,7 +18,7 @@ class StorageProvider {
             $config = Get-Content -Path $path -Raw | ConvertFrom-Metadata
             return $config
         } else {
-            Write-Error "Configuration [$path] not found"
+            Write-Warning -Message "Configuration [$path] not found"
             return $null
         }
     }
