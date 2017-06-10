375778e6be490a713ee912fee3360961d9bcd83b
Brandon Olin <brandon@devblackops.io>
2017-06-10 14:12:24 -0700

    Squash error when trying to remove job on shutdown

--- PoshBot/Implementations/Slack/SlackConnection.ps1
+++ PoshBot/Implementations/Slack/SlackConnection.ps1
@@ -100,7 +100,7 @@ class SlackConnection : Connection {
     # Stop the receive job
     [void]Disconnect() {
         Write-Verbose -Message '[SlackConnection:Disconnect] Closing websocket'
-        $this.ReceiveJob | Stop-Job -Confirm:$false -PassThru | Remove-Job -Force
+        $this.ReceiveJob | Stop-Job -Confirm:$false -PassThru | Remove-Job -Force -ErrorAction SilentlyContinue
         $this.Connected = $false
         $this.Status = [ConnectionStatus]::Disconnected
     }
