9c8c66b24a58ad2340da5340993aa06889794389
Brandon Olin <brandon@devblackops.io>
2018-07-27 23:42:55 -0700

    Use one version of required Service Bus libraries for Teams backend

--- PoshBot/LoadDLLs.ps1
+++ PoshBot/LoadDLLs.ps1
@@ -1,14 +1,11 @@
 
 if (($null -eq $IsWindows) -or $IsWindows) {
-    $platform = 'windows'
-    Add-Type -Path "$PSScriptRoot/lib/$platform/netstandard.dll"
-} else {
-    $platform = 'linux'
+    Add-Type -Path "$PSScriptRoot/lib/netstandard.dll"
 }
 
 @(
-    Resolve-Path -Path "$PSScriptRoot/lib/$platform/Microsoft.Azure.Amqp.dll"
-    Resolve-Path -Path "$PSScriptRoot/lib/$platform/Microsoft.Azure.ServiceBus.dll"
+    Resolve-Path -Path "$PSScriptRoot/lib/Microsoft.Azure.Amqp.dll"
+    Resolve-Path -Path "$PSScriptRoot/lib/Microsoft.Azure.ServiceBus.dll"
 ) | ForEach-Object {
     [Void][System.Reflection.Assembly]::LoadFrom($_)
 }
