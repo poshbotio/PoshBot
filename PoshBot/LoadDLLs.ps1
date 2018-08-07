100cb6e3f84b8f08be9675c0e1c87fe3d9e2465c
Brandon Olin <brandon@devblackops.io>
2018-08-06 21:50:45 -0700

    Load Service Bus DLLs with Add-Type

--- PoshBot/LoadDLLs.ps1
+++ PoshBot/LoadDLLs.ps1
@@ -1,13 +1,24 @@
-if (($null -eq $IsWindows) -or $IsWindows) {
-    $platform = 'windows'
-    Add-Type -Path "$PSScriptRoot/lib/$platform/netstandard.dll"
-} else {
-    $platform = 'linux'
-}
+# Load Service Bus DLLs
+# Windows PowerShell will use full .Net DLLs, PowerShell Core will use .Net core DLLs
+try {
+    if ($PSVersionTable.PSEdition -eq 'Desktop') {
+        $platform = 'windows'
+        # [System.Reflection.Assembly]::LoadFrom((Resolve-Path "$PSScriptRoot/lib/$platform/Microsoft.IdentityModel.Clients.ActiveDirectory.dll")) > $null
+        # [System.Reflection.Assembly]::LoadFrom((Resolve-Path "$PSScriptRoot/lib/$platform/Microsoft.ServiceBus.dll")) > $null
+        # Add-Type -Path "$PSScriptRoot/lib/$platform/netstandard.dll"
+        # Add-Type -Path "$PSScriptRoot/lib/$platform/Microsoft.Azure.Amqp.dll"
+        # Add-Type -Path "$PSScriptRoot/lib/$platform/Microsoft.Azure.ServiceBus.dll"
 
- @(
-    Resolve-Path -Path "$PSScriptRoot/lib/$platform/Microsoft.Azure.Amqp.dll"
-    Resolve-Path -Path "$PSScriptRoot/lib/$platform/Microsoft.Azure.ServiceBus.dll"
-) | ForEach-Object {
-    [Void][System.Reflection.Assembly]::LoadFrom($_)
+        Add-Type -Path "$PSScriptRoot/lib/$platform/netstandard.dll"
+        Add-Type -Path "$PSScriptRoot/lib/$platform/System.Diagnostics.DiagnosticSource.dll"
+        #Add-Type -Path "$modulePath/lib/$platform/System.IdentityModel.Tokens.Jwt.dll"
+        Add-Type -Path "$PSScriptRoot/lib/$platform/Microsoft.Azure.Amqp.dll"
+        Add-Type -Path "$PSScriptRoot/lib/$platform/Microsoft.Azure.ServiceBus.dll"
+    } else {
+        $platform = 'linux'
+        Add-Type -Path "$PSScriptRoot/lib/$platform/Microsoft.Azure.Amqp.dll"
+        Add-Type -Path "$PSScriptRoot/lib/$platform/Microsoft.Azure.ServiceBus.dll"
+    }
+} catch {
+    throw $_
 }
