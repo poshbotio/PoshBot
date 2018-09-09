9aec1d6454421c36fe633d472073ffc17fc4b96f
Brandon Olin <brandon@devblackops.io>
2018-09-09 15:46:13 -0700

    Handle removing multiple groups or roles at once

--- PoshBot/Plugins/Builtin/Public/Remove-Group.ps1
+++ PoshBot/Plugins/Builtin/Public/Remove-Group.ps1
@@ -20,17 +20,71 @@ function Remove-Group {
         $Bot,
 
         [parameter(Mandatory, Position = 0)]
-        [string]$Name
+        [string[]]$Name
     )
 
-    if ($g = $Bot.RoleManager.GetGroup($Name)) {
-        try {
-            $Bot.RoleManager.RemoveGroup($g)
-            New-PoshBotCardResponse -Type Normal -Text "Group [$Name] removed" -ThumbnailUrl $thumb.success
-        } catch {
-            New-PoshBotCardResponse -Type Error -Text "Failed to remove group [$Name]" -ThumbnailUrl $thumb.error
-        }
-    } else {
-        New-PoshBotCardResponse -Type Warning -Text "Group [$Name] not found :(" -Title 'Rut row' -ThumbnailUrl $thumb.rutrow
+    $removed = @()
+    $notFound = @()
+    $failedToRemove = @()
+    $response = @{
+        Type         = 'Normal'
+        Text         = ''
+        Title        = $null
+        ThumbnailUrl = $thumb.success
     }
+
+    # Remove group(s)
+    foreach ($groupName in $Name) {
+        if ($g = $Bot.RoleManager.GetGroup($groupName)) {
+            $Bot.RoleManager.RemoveGroup($g)
+            if ($g = $Bot.RoleManager.GetGroup($groupName)) {
+                $failedToRemove += $groupName
+            } else {
+                $removed += $groupName
+            }
+        } else {
+            $notFound += $groupName
+        }
+    }
+
+    # Send success messages
+    if ($removed.Count -ge 1) {
+        if ($removed.Count -gt 1) {
+            $successMessage = 'Groups [{0}] removed.' -f ($removed -join ', ')
+        } else {
+            $successMessage = "Group [$removed] removed"
+        }
+        $response.Type = 'Normal'
+        $response.Text = $successMessage
+        $response.Title = $null
+        $response.ThumbnailUrl = $thumb.success
+    }
+
+    # Send warning messages
+    if ($notFound.Count -ge 1) {
+        if ($notFound.Count -gt 1) {
+            $warningMessage = 'Groups [{0}] not found :(' -f ($removed -join ', ')
+        } else {
+            $warningMessage = "Group [$notFound] not found :("
+        }
+        $response.Type = 'Warning'
+        $response.Text = $warningMessage
+        $response.Title = $null
+        $response.ThumbnailUrl = $thumb.rutrow
+    }
+
+    # Send failure messages
+    if ($failedToRemove.Count -ge 1) {
+        if ($failedToRemove.Count -gt 1) {
+            $errMsg = "Groups [{0}] could not be removed. Check logs for more information." -f ($failedToRemove -join ', ')
+        } else {
+            $errMsg = "Group [$failedToRemove] could not be created. Check logs for more information."
+        }
+        $response.Type = 'Error'
+        $response.Text = $errMsg
+        $response.Title = $null
+        $response.ThumbnailUrl = $thumb.error
+    }
+
+    New-PoshBotCardResponse @response
 }
--- PoshBot/Plugins/Builtin/Public/Remove-Role.ps1
+++ PoshBot/Plugins/Builtin/Public/Remove-Role.ps1
@@ -18,17 +18,71 @@ function Remove-Role {
         $Bot,
 
         [parameter(Mandatory, Position = 0)]
-        [string]$Name
+        [string[]]$Name
     )
 
-    if ($r = $Bot.RoleManager.GetRole($Name)) {
-        try {
-            $Bot.RoleManager.RemoveRole($r)
-            New-PoshBotCardResponse -Type Normal -Text "Role [$Name] removed" -ThumbnailUrl $thumb.success
-        } catch {
-            New-PoshBotCardResponse -Type Error -Text "Failed to remove role [$Name]" -ThumbnailUrl $thumb.error
-        }
-    } else {
-        New-PoshBotCardResponse -Type Warning -Text "Role [$Name] not found :(" -Title 'Rut row' -ThumbnailUrl $thumb.rutrow
+    $removed = @()
+    $notFound = @()
+    $failedToRemove = @()
+    $response = @{
+        Type         = 'Normal'
+        Text         = ''
+        Title        = $null
+        ThumbnailUrl = $thumb.success
     }
+
+    # Remove role(s)
+    foreach ($roleName in $Name) {
+        if ($r = $Bot.RoleManager.GetRole($roleName)) {
+            $Bot.RoleManager.RemoveRole($r)
+            if ($r = $Bot.RoleManager.GetRole($roleName)) {
+                $failedToRemove += $roleName
+            } else {
+                $removed += $roleName
+            }
+        } else {
+            $notFound += $roleName
+        }
+    }
+
+    # Send success messages
+    if ($removed.Count -ge 1) {
+        if ($removed.Count -gt 1) {
+            $successMessage = 'Roles [{0}] removed.' -f ($removed -join ', ')
+        } else {
+            $successMessage = "Role [$removed] removed"
+        }
+        $response.Type = 'Normal'
+        $response.Text = $successMessage
+        $response.Title = $null
+        $response.ThumbnailUrl = $thumb.success
+    }
+
+    # Send warning messages
+    if ($notFound.Count -ge 1) {
+        if ($notFound.Count -gt 1) {
+            $warningMessage = 'Roles [{0}] not found :(' -f ($removed -join ', ')
+        } else {
+            $warningMessage = "Role [$notFound] not found :("
+        }
+        $response.Type = 'Warning'
+        $response.Text = $warningMessage
+        $response.Title = $null
+        $response.ThumbnailUrl = $thumb.rutrow
+    }
+
+    # Send failure messages
+    if ($failedToRemove.Count -ge 1) {
+        if ($failedToRemove.Count -gt 1) {
+            $errMsg = "Roles [{0}] could not be removed. Check logs for more information." -f ($failedToRemove -join ', ')
+        } else {
+            $errMsg = "Role [$failedToRemove] could not be created. Check logs for more information."
+        }
+        $response.Type = 'Error'
+        $response.Text = $errMsg
+        $response.Title = $null
+        $response.ThumbnailUrl = $thumb.error
+    }
+
+    New-PoshBotCardResponse @response
 }
