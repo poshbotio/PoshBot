f112b1a5214a7b41246012ea2e585ee1611e9d40
Brandon Olin <brandon@devblackops.io>
2018-10-06 15:08:50 -0700

    Create buffer as an [ArraySegment[byte[]]
    This is to satisfy the
    System.Net.WebSockets.ClientWebSocket.ReceiveAsync() method so we don't
    get ambiguous method signature errors.

--- PoshBot/Implementations/Slack/SlackConnection.ps1
+++ PoshBot/Implementations/Slack/SlackConnection.ps1
@@ -62,7 +62,7 @@ class SlackConnection : Connection {
             until ($task.IsCompleted)
 
             # Receive messages and put on output stream so the backend can read them
-            $buffer = (New-Object System.Byte[] 4096)
+            [ArraySegment[byte]]$buffer = [byte[]]::new(4096)
             $ct = New-Object System.Threading.CancellationToken
             $taskResult = $null
             while ($webSocket.State -eq [System.Net.WebSockets.WebSocketState]::Open) {
