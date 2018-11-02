c1817c07ec080fb639b19bdb000213f37edb56ae
Brandon Olin <brandon@devblackops.io>
2018-11-01 20:51:53 -0700

    Only require SLACK_TOKEN and BOT_ADMINS if creating cfg for first time

--- docker/docker_entrypoint.ps1
+++ docker/docker_entrypoint.ps1
@@ -34,37 +34,35 @@ if ($IsLinux -or $IsMacOs) {
 
 $VerbosePreference = 'Continue'
 
-# SLACK_TOKEN and BOT_ADMINS environment variables are REQUIRED
-# If they where not passed in then bail
-$slackToken = Get-FromEnv -Name 'SLACK_TOKEN' -Default [string]::empty
-$botAdmins  = Get-FromEnv -Name 'BOT_ADMINS'  -Default @([string]::empty)
-if ([string]::IsNullOrEmpty($slackToken) -or $botAdmins.Count -eq 0) {
-    throw 'Please specify your Slack token and initial list of bot administrators'
-    #exit 1
-}
-
 Import-Module -Name PoshBot -ErrorAction Stop -Verbose:$false
 
 $cmdPrefix          = Get-FromEnv -Name 'POSHBOT_CMD_PREFIX'            -Default '!'
 $altCmdPrefixes     = Get-FromEnv -Name 'POSHBOT_ALT_CMD_PREFIXES'      -Default 'poshbot'
 $altCmdPrefixes     = $altCmdPrefixes -split ';'
-$pluginRepos        = Get-FromEnv -Name 'POSHBOT_PLUGIN_REPOSITORIES'   -Default 'PSGallery'
-$pluginRepos = $pluginRepos -split ';'
+$pluginRepos        = (Get-FromEnv -Name 'POSHBOT_PLUGIN_REPOSITORIES'   -Default 'PSGallery') -split ';'
 $logDir             = Get-FromEnv -Name 'POSHBOT_LOG_DIR'               -Default "$rootDrive/poshbot_data/logs"
 $botName            = Get-FromEnv -Name 'POSHBOT_NAME'                  -Default 'PoshBot_Docker'
 $confDir            = Get-FromEnv -Name 'POSHBOT_CONF_DIR'              -Default "$rootDrive/poshbot_data"
 $logLevel           = Get-FromEnv -Name 'POSHBOT_LOG_LEVEL'             -Default 'Verbose'
 $pluginDir          = Get-FromEnv -Name 'POSHBOT_PLUGIN_DIR'            -Default "$rootDrive/poshbot_data/plugins"
 $muteUnknownCommand = Get-FromEnv -Name 'POSHBOT_MUTE_UNKNOWN_CMD'      -Default $false
-$manifestsToLoad    = Get-FromEnv -Name 'POSHBOT_MANIFESTS_TO_LOAD'     -Default @()
-$manifestsToLoad = $manifestsToLoad -split ';'
-$altCmdPrefixSep    = Get-FromEnv -Name 'POSHBOT_ALT_CMD_PREFIX_SEP'    -Default @(':',',',';')
-$altCmdPrefixSep = $altCmdPrefixSep.ToCharArray()
-$sendCmdRespToPriv  = Get-FromEnv -Name 'POSHBOT_SEND_CMD_RESP_TO_PRIV' -Default @()
-$sendCmdRespToPriv = $sendCmdRespToPriv -split ';'
+$manifestsToLoad    = (Get-FromEnv -Name 'POSHBOT_MANIFESTS_TO_LOAD'     -Default @()) -split ';'
+$altCmdPrefixSep    = (Get-FromEnv -Name 'POSHBOT_ALT_CMD_PREFIX_SEP'    -Default @(':',',',';')).ToCharArray()
+$sendCmdRespToPriv  = (Get-FromEnv -Name 'POSHBOT_SEND_CMD_RESP_TO_PRIV' -Default @()) -split ';'
 
 $configPSD1 = (Join-Path -Path $confDir -ChildPath 'PoshBot.psd1')
 if (-not (Test-Path -Path $configPSD1)) {
+
+    # If creating a PoshBot config for the first time, then
+    # SLACK_TOKEN and BOT_ADMINS environment variables are REQUIRED.
+    # If they where not passed in then bail.
+    $slackToken = Get-FromEnv -Name 'SLACK_TOKEN' -Default ''
+    $botAdmins  = Get-FromEnv -Name 'BOT_ADMINS'  -Default @()
+    if ([string]::IsNullOrEmpty($slackToken) -or $botAdmins.Count -eq 0) {
+        throw 'SLACK_TOKEN and BOT_ADMINS environment variables are required in order to create working PoshBot configuration. Please specify your Slack token and initial list of bot administrators.'
+        exit 1
+    }
+
     # Create the initial configuration
     $hash = @{
         Name                             = $botName
@@ -73,10 +71,6 @@ if (-not (Test-Path -Path $configPSD1)) {
         PluginRepository                 = $pluginRepos
         LogDirectory                     = $logDir
         BotAdmins                        = $botAdmins
-        BackendConfiguration             = @{
-            Token = $slackToken
-            Name  = 'SlackBackend'
-        }
         LogLevel                         = $logLevel
         AlternateCommandPrefixes         = $altCmdPrefixes
         PluginDirectory                  = $pluginDir
@@ -84,7 +78,11 @@ if (-not (Test-Path -Path $configPSD1)) {
         ModuleManifestsToLoad            = $manifestsToLoad
         AlternateCommandPrefixSeperators = $altCmdPrefixSep
         SendCommandResponseToPrivate     = $sendCmdRespToPriv
-        PluginConfiguration = @{}
+        PluginConfiguration              = @{}
+        BackendConfiguration             = @{
+            Token = $slackToken
+            Name  = 'SlackBackend'
+        }
     }
     $config = New-PoshBotConfiguration @hash -Verbose
     if (-not (Test-Path -Path $confDir)) {
@@ -95,17 +93,13 @@ if (-not (Test-Path -Path $configPSD1)) {
 } else {
     # There was a previous configuraiton
     # Merge any values from env vars into config
-    $config = Import-PowerShellDataFile -Path $configPSD1
+    $config = Get-PoshBotConfiguration -Path $configPSD1
     $config.Name                             = $botName
     $config.ConfigurationDirectory           = $confDir
     $config.CommandPrefix                    = $cmdPrefix
     $config.PluginRepository                 = $pluginRepos
     $config.LogDirectory                     = $logDir
     $config.BotAdmins                        = $botAdmins
-    $config.BackendConfiguration             = @{
-        Token = $slackToken
-        Name  = 'SlackBackend'
-    }
     $config.LogLevel                         = $logLevel
     $config.AlternateCommandPrefixes         = $altCmdPrefixes
     $config.PluginDirectory                  = $pluginDir
@@ -113,6 +107,10 @@ if (-not (Test-Path -Path $configPSD1)) {
     $config.ModuleManifestsToLoad            = $manifestsToLoad
     $config.AlternateCommandPrefixSeperators = $altCmdPrefixSep
     $config.SendCommandResponseToPrivate     = $sendCmdRespToPriv
+    $config.BackendConfiguration             = @{
+        Token = $slackToken
+        Name  = 'SlackBackend'
+    }
 
     $config | Save-PoshBotConfiguration -Path $configPSD1 -Force -Verbose
     $pbc = Get-PoshBotConfiguration -Path $configPSD1 -Verbose
