3d1059c03aa842e3a18c46fedfa451881e9a7893
Nate Ferrell <nate@scrthq.com>
2019-10-05 00:23:18 -0500

    added variations to !slap

--- PoshBot/Plugins/Builtin/Public/Slap.ps1
+++ PoshBot/Plugins/Builtin/Public/Slap.ps1
@@ -6,15 +6,45 @@ function Slap {
         The user that will be slapped
     .EXAMPLE
         !slap --user jaap
+
+    .EXAMPLE
+        !slap jaap foamfinger
     #>
     [cmdletbinding()]
     param(
         [parameter(Mandatory)]
-        $Bot,    
+        $Bot,
 
         [parameter(Mandatory, Position = 0)]
-        [string] $User
-    )
+        [string] $User,
 
-    New-PoshBotCardResponse -Type Normal -Text "slaps $User around a bit with a large trout" -ThumbnailUrl 'http://i.imgur.com/W1Dkemu.jpg'
+        [parameter(Position = 1)]
+        [string] $Object
+    )
+    $objects = @{
+        trout = @{
+            item = 'large trout'
+            thumbnail = 'https://upload.wikimedia.org/wikipedia/commons/1/16/Rainbow_trout_transparent.png'
+        }
+        foamfinger = @{
+            item = 'giant foam finger'
+            thumbnail = 'https://images.vexels.com/media/users/3/153013/isolated/preview/517c07f5ff433028345e10b138870119-american-foam-finger-design-element-by-vexels.png'
+        }
+        keyboard = @{
+            item = 'mechanical keyboard'
+            thumbnail = 'https://cdn.pixabay.com/photo/2013/07/13/11/50/computer-158770_960_720.png'
+        }
+        foamsword = @{
+            item = 'foam sword'
+            thumbnail = 'https://upload.wikimedia.org/wikipedia/commons/2/2b/Foamswordofrecall.png'
+        }
+    }
+    $choice = if ($PSBoundParameters.ContainsKey('Object') -and $objects.ContainsKey($Object)) {
+        $objects[$Object]
+    }
+    else {
+        $random = $objects.Keys | Get-Random
+        $objects[$random]
+    }
+    New-PoshBotCardResponse -Type Normal -Text "slaps $User around a bit with a $($choice['item'])" -ThumbnailUrl $choice['thumbnail']
 }
