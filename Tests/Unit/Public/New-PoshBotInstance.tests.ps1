5d99563b299872f71bdde9c12e8f4b51c9991ac8
Brandon Olin <brandon@devblackops.io>
2020-03-30 20:24:02 -0700

    fix tests

--- Tests/Unit/Public/New-PoshBotInstance.tests.ps1
+++ Tests/Unit/Public/New-PoshBotInstance.tests.ps1
@@ -6,51 +6,48 @@ Describe 'New-PoshBotInstance' {
         $PSDefaultParameterValues = @{
             'New-PoshBotInstance:Verbose' = $false
         }
+        $script:psd1    = Get-Item -Path (Join-Path -Path $PSScriptRoot -ChildPath '..\..\Artifacts\Cherry2000.psd1')
         $script:backend = New-PoshBotSlackBackend -Configuration @{Token = (New-Guid).ToString(); Name = 'Test-Backend'}
+        $script:bot     = New-PoshBotInstance -Path $psd1 -Backend $script:backend -WarningAction Ignore
     }
 
     AfterAll {
-        $bot.Dispose()
+        $script:bot.Dispose()
     }
 
     Context 'Path' {
         it 'Creates a bot instance from a configuration path' {
-            $psd1 = Get-Item -Path (Join-Path -Path $PSScriptRoot -ChildPath '..\..\Artifacts\Cherry2000.psd1')
-            $bot = New-PoshBotInstance -Path $psd1 -Backend $script:backend
-            $bot | should not benullorempty
-            $bot.PSObject.TypeNames[0] | should be 'Bot'
+            $script:bot | Should -Not -BeNullOrEmpty
+            $script:bot.PSObject.TypeNames[0] | Should -Be 'Bot'
         }
 
         it 'Accepts paths from pipeline' {
-            $bot = (Join-Path -Path $PSScriptRoot -ChildPath '..\..\Artifacts\Cherry2000.psd1') | New-PoshBotInstance -Backend $script:backend
-            $bot | should not benullorempty
-            $bot.PSObject.TypeNames[0] | should be 'Bot'
+            $script:bot = $script:psd1 | New-PoshBotInstance -Backend $script:backend -WarningAction Ignore
+            $script:bot | Should -Not -BeNullOrEmpty
+            $script:bot.PSObject.TypeNames[0] | Should -Be 'Bot'
         }
 
         it 'Validates path and file type' {
-            {New-PoshBotInstance -Path '.\nonexistentfile.asdf' -Backend $script:backend} | should throw
+            {New-PoshBotInstance -Path '.\nonexistentfile.asdf' -Backend $script:backend} | Should -Throw
         }
     }
 
     Context 'LiteralPath' {
         it 'Accepts LiteralPath' {
-            $psd1 = Join-Path -Path $PSScriptRoot -ChildPath '..\..\Artifacts\Cherry2000.psd1'
-            $config = New-PoshBotInstance -LiteralPath $psd1 -Backend $script:backend
-            $config | should not benullorempty
+            $config = New-PoshBotInstance -LiteralPath $script:psd1 -Backend $script:backend -WarningAction Ignore
+            $config | Should -Not -BeNullOrEmpty
         }
     }
 
     Context 'Configuration' {
         it 'Accepts configuration object' {
-            $psd1 = Join-Path -Path $PSScriptRoot -ChildPath '..\..\Artifacts\Cherry2000.psd1'
-            $config = $psd1 | Get-PoshBotConfiguration
-            $bot = New-PoshBotInstance -Configuration $config -Backend $script:backend
+            $config = $script:psd1 | Get-PoshBotConfiguration
+            {$script:bot = New-PoshBotInstance -Configuration $config -Backend $script:backend -WarningAction Ignore} | Should -Not -Throw
         }
 
         it 'Accepts configuration object from pipeline' {
-            $psd1 = Join-Path -Path $PSScriptRoot -ChildPath '..\..\Artifacts\Cherry2000.psd1'
-            $config = $psd1 | Get-PoshBotConfiguration
-            $bot = $config | New-PoshBotInstance -Backend $script:backend
+            $config = $script:psd1 | Get-PoshBotConfiguration
+            {$script:bot = $config | New-PoshBotInstance -Backend $script:backend -WarningAction Ignore} | Should -Not -Throw
         }
     }
 }
