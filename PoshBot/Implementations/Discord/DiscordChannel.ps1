bc7a0aac52057088a9e12b2568238b5b61ac4b56
Joshua King <windosbk+github@gmail.com>
2022-02-05 13:40:49 +1300

    (#241) Add additional Discord channel types

--- PoshBot/Implementations/Discord/DiscordChannel.ps1
+++ PoshBot/Implementations/Discord/DiscordChannel.ps1
@@ -1,11 +1,15 @@
 enum DiscordChannelType {
-    GUILD_TEXT     = 0
-    DM             = 1
-    GUILD_VOICE    = 2
-    GROUP_DM       = 3
-    GUILD_CATEGORY = 4
-    GUILD_NEWS     = 5
-    GUILD_STORE    = 6
+    GUILD_TEXT           = 0
+    DM                   = 1
+    GUILD_VOICE          = 2
+    GROUP_DM             = 3
+    GUILD_CATEGORY       = 4
+    GUILD_NEWS           = 5
+    GUILD_STORE          = 6
+    GUILD_NEWS_THREAD    = 10
+    GUILD_PUBLIC_THREAD  = 11
+    GUILD_PRIVATE_THREAD = 12
+    GUILD_STAGE_VOICE    = 13
 }
 
 class DiscordChannel : Room {
